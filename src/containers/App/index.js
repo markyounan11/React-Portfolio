185d68a79754c54c1e7a3a2a24fdee6eef2a0981
Mark Younan <markyounan@gmail.com>
2020-04-09 11:23:42 -0700

    added boiler plate. Changed folders and file names.

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from './About';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+class App extends Component {
+    render() {
+      return (
+        <div className="container">
+          
+        </div>
+      );
+    }
+  }
+  export default App;
