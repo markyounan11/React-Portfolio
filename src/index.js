a73d707d6d71bc4be0bde863c7eb2a10ac5936f3
Mark Younan <markyounan@gmail.com>
2020-06-15 13:36:33 -0700

    updated index.js inside of src

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
+import Footer from "./components/Footer";
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render([<App key="1" />, <Footer key="2" />], document.getElementById("root"));
