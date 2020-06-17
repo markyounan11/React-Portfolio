86b823873eed79bd2df9f328c5dde48eb1798864
Mark Younan <markyounan@gmail.com>
2020-06-17 10:16:02 -0700

    added stuff to gitignore

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import About from '../About';
 import Contact from '../../containers/Contact';
-// import Navbar from '../Navbar';
-// import Footer from '../Footer';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 // import Portfolio from '../Portfolio';
 
 
@@ -12,11 +12,11 @@ import Contact from '../../containers/Contact';
 
 const App = props => (
   <Router>
-    {/* <Navbar /> */}
+    <Navbar />
     <Route exact path="/" component={About}/>
     <Route exact path="/Contact" component={Contact}/>
     {/* <Route exact path="/Portfolio" component={Portfolio}/> */}
-    {/* <Footer/> */}
+    <Footer/>
   </Router>
 );
 
