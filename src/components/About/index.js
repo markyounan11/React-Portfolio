826ce1f634e4a33fa65aa12140629b55e4d4969a
Mark Younan <markyounan@gmail.com>
2020-08-13 13:27:19 -0700

    updated paragraph and spacing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,18 +21,23 @@ const About = props => (
                     <p>
                         Thank you for visiting my website! My name is Mark Younan and I am a Full Stack Software Engineer in the San Francisco Bay area. My whole life, I've always had a fascination with technology and figuring out new, more efficient ways to complete every day tasks in order to make life easier. I am a self-motivated problem solver and challenge seeker who thrives best when working in a team. I merge a passion for usability and user experience with technical knowledge to create seamless digital experiences. I’m eager to always be learning new technologies and frameworks. When I am not at work, I enjoy playing golf, watching basketball, going to the beach and being out in nature. I'm also into super cars and I love and spending time with my family.
 
-Front End: JavaScript, jQuery, JSX, HTML, CSS, Bootstrap, Bulma, Semantic UI, Semantic UI React, APIs, JSON, AJAX, Axios, React.js, Redux.js
-
-Back End: Node.js, Express.js, MVC, ORM (Sequelize), Creating APIs, User Authentication, MySQL, MongoDB
-
-Dev Tools: Git, Github, Gitlab, NPM, React Developer Tools, Redux DevTools, Heroku, Webpack
-
-CS Fundamentals: Algorithms, Data Structures      
+                    </p>
+                    <p>
+                        Front End: JavaScript, jQuery, JSX, HTML, CSS, Bootstrap, Bulma, Semantic UI, Semantic UI React, APIs, JSON, AJAX, Axios, React.js, Redux.js
+                    </p>
+                    <p>
+                        Back End: Node.js, Express.js, MVC, ORM (Sequelize), Creating APIs, User Authentication, MySQL, MongoDB
+                    </p>
+                    <p>
+                        Dev Tools: Git, Github, Gitlab, NPM, React Developer Tools, Redux DevTools, Heroku, Webpack
+                    </p>
+                    <p>
+                        CS Fundamentals: Algorithms, Data Structures
                     </p>
                 </div>
             </div>
-                </div>
-            </div>
+        </div>
+    </div>
 );
 
 export default About;
